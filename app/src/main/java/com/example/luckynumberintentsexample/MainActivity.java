6b7862e49e777a954d1a11b91bacd8e2539dee34
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-08-27 10:46:54 +0530

    Created the Design now to focus on Intent and how to send data

--- app/src/main/java/com/example/luckynumberintentsexample/MainActivity.java
+++ app/src/main/java/com/example/luckynumberintentsexample/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.luckynumberintentsexample;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    EditText name;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +27,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        name=findViewById(R.id.enterName);
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userName = name.getText().toString();
+
+                //Explicit Intent
+
+                Intent i = new Intent(getApplicationContext(), SecondActivity.class);
+                i.putExtra()
+            }
+        });
     }
+
+
+
 }
